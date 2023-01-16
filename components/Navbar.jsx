ae004d95b74c68458b846b7023b26c4c90f24794
TanaTTario <122384343+nititornBS@users.noreply.github.com>
2023-01-16 22:28:06 +0700

    start up

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between item-center w-full h-full px-2 2xl:px-16">
+    <Image src="..,public/assets/navLogo.png" alt="" />
+      </div>
+    </div>
+  )}
---
+++ src/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Clint | Front-End Developer</title>
+        
+      </head>
+      {/* <Navber> */}
+    </div>
+  )
+}
